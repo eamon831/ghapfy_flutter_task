df717b537185398e7371b06caa9101af09dc23d5
MD SAIFUL HOSSAIN <mdsaiful.rbs@gmail.com>
2024-10-21 18:17:21 +0600

    Initial setup on going

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,17 +1,15 @@
-import 'package:flutter/material.dart';
+import '/app/core/exporter.dart';
 
 import '/app/my_app.dart';
-import '/flavors/build_config.dart';
 import '/flavors/env_config.dart';
 import '/flavors/environment.dart';
 import 'app/bindings/initial_binding.dart';
-import 'app/core/session_manager/session_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   /// uncomment below 2 lines to set the orientation of the app
-  //await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
   final devConfig = EnvConfig(
     appName: 'Flutter GetX Template Dev',
